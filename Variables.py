ce8454ce90b4e1d2d45b2d364c9f854fb1dd56f0
OmarTerreros <yairterrerosmtz@gmail.com>
2025-06-08 23:30:17 +0000

    Agregando Variables y quitando lineas de ignore

---
+++ Variables.py
@@ -0,0 +1,11 @@
+nombre= input("Escribe tu nombre:")
+
+nombre1=nombre.istitle()
+print (nombre1)
+
+if nombre1== True:
+    print("Tu Nombre esta bien")
+elif nombre1 == False:
+    print("Tu nombre esta mal")
+    nombre2=nombre.title()
+    print(f"Tu nombre es asi:{nombre2}")
